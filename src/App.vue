8654473266e85bfd9dc4a54b9a494a64e53bc194
reganputra <ramadhanregan334@gmail.com>
2025-04-18 08:15:10 +0700

    setup project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Welcome to My Vue App</h1>
+    <p>This is a simple Vue.js application.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
