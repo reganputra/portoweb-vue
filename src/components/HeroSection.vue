916b4992165ee40dd549c4f5f207180d86df2b79
reganputra <ramadhanregan334@gmail.com>
2025-04-19 15:29:20 +0700

    add: hero section component

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="relative w-full p-3 bg-primary2">
+    <div class=" grid grid-cols-1 md:grid-clos-2 lg:grid-clos-4 gap-4 ">
+      <div class="col-span-1 order-2 md:order-1 lg:order-1 rounded-3x1 p-5 bg-white">
+
+      </div>
+      <div class="col-span-1 order-2  md:col-span-1 lg:col-span-2 rounded-3x1 p-4 bg-white">
+
+      </div>
+      <div class="col-span-1 order-3 md:order-3 lg:order-3 flex justify-center rounded-3x1 p-4 bg-white">
+
+      </div>
+    </div>
+  </section>
+</template>
